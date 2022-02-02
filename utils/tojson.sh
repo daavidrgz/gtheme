da0baa76b567f223db24bf4e4e23a21fe6938dd3
David <david.rbacelar@udc.es>
2022-02-02 18:45:21 +0100

    Update

--- utils/tojson.sh
+++ utils/tojson.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-THEMES_PATH="/home/david/.config/gtheme-rust/themes"
-DEST_PATH="/home/david/.config/gtheme-rust/json-themes"
+THEMES_PATH=$HOME"/.config/gtheme-rust/themes"
+DEST_PATH=$HOME"/.config/gtheme-rust/json-themes"
 
 ATTR_NAMES=("background" "foreground" "cursor" "selection-background" "selection-foreground" \
 "black" "black-hg" "red" "red-hg" "green" "green-hg" "yellow" "yellow-hg" "blue" "blue-hg" "magenta" "magenta-hg" "cyan" "cyan-hg" "white" "white-hg" "vscode" "wallpaper")
 
 for f in $THEMES_PATH/*; do
 	DEST=$DEST_PATH/$(echo $(basename $f) | sed 's/.colors/.json/g')
-	cp /home/david/.config/gtheme-rust/colors.json $DEST
+	cp $HOME/.config/gtheme-rust/theme.json $DEST
   declare CONTENT="$(/bin/cat $f)"
 	for attr in ${ATTR_NAMES[@]}; do
     declare VALUE=$(echo "$CONTENT" | grep -e "^$attr:.*" | awk -F ': ' '{print $2}')
