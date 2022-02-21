2a1a6e0a6539f7915f41355be0fe72a79a008f32
David <david.rbacelar@udc.es>
2022-02-21 17:51:27 +0100

    Update

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,35 +1,35 @@
 use clap::{Command, Arg};
 
 fn main() {
-	let mut app = Command::new("gtheme")
-		.version("1.0")
-		.about("A rust program that makes your theming life so much easier.")
-		.author("David Rodríguez & Jorge Hermo");
+// 	let mut app = Command::new("gtheme")
+// 		.version("1.0")
+// 		.about("A rust program that makes your theming life so much easier.")
+// 		.author("David Rodríguez & Jorge Hermo");
 
-	let apply_theme = Arg::new("theme")
-		.short('t')
-		.long("theme") // allow --name
-		.takes_value(true)
-		.help("Apply specified theme")
-		.exclusive(true);
+// 	let apply_theme = Arg::new("theme")
+// 		.short('t')
+// 		.long("theme") // allow --name
+// 		.takes_value(true)
+// 		.help("Apply specified theme")
+// 		.exclusive(true);
 
-let activate_patterns = Arg::new("activate")
-		.short('a')
-		.long("activate")
-		.takes_value(true) // allow --name
-		.multiple_values(true)
-		.value_name("pattern")
-		.help("Activate specified patterns in the current desktop")
-		.exclusive(true);
+// let activate_patterns = Arg::new("activate")
+// 		.short('a')
+// 		.long("activate")
+// 		.takes_value(true) // allow --name
+// 		.multiple_values(true)
+// 		.value_name("pattern")
+// 		.help("Activate specified patterns in the current desktop")
+// 		.exclusive(true);
 
-	app = app.args([apply_theme, activate_patterns]);
+// 	app = app.args([apply_theme, activate_patterns]);
 
-	let matches = app.get_matches();
+// 	let matches = app.get_matches();
 
-	match matches.value_of("theme") {
-		Some(theme_name) => println!("{}", theme_name),
-		None => println!("There is no theme specified!")
-	};
+// 	match matches.value_of("theme") {
+// 		Some(theme_name) => println!("{}", theme_name),
+// 		None => println!("There is no theme specified!")
+// 	};
 
-  // app::Ui::default().start_ui()
+  app::Ui::default().start_ui()
 }
