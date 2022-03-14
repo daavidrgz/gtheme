52dd648fb945f266cb4ce41a608e997c0fff6d32
David <david.rbacelar@udc.es>
2022-03-14 21:20:29 +0100

    Add: Cli manpage

--- build.rs
+++ build.rs
@@ -1,29 +1,29 @@
 use clap_complete::{generate_to, shells::Shell};
-use std::{
-    env, fs,
-    io::Result,
-    path::{Path, PathBuf},
-};
+use clap_mangen::Man;
+use std::{ env, fs, io::Result };
 
 include!("src/cli/commands.rs");
 
 fn main() -> Result<()> {
-			let out_dir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("completions/");
+			let completions_dir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("completions/");
+			let manpage_dir = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("manpage/");
+			let _ = fs::create_dir(&completions_dir);
+			let _ = fs::create_dir(&manpage_dir);
 
 			// Generate completions
 			let mut app = build_app();
-			generate_to(Shell::Bash, &mut app, "gtheme", &out_dir)?;
-			generate_to(Shell::Zsh, &mut app, "gtheme", &out_dir)?;
-			generate_to(Shell::Fish, &mut app, "gtheme", &out_dir)?;
-			generate_to(Shell::PowerShell, &mut app, "gtheme", &out_dir)?;
-			generate_to(Shell::Elvish, &mut app, "gtheme", &out_dir)?;
+			generate_to(Shell::Bash, &mut app, "gtheme", &completions_dir)?;
+			generate_to(Shell::Zsh, &mut app, "gtheme", &completions_dir)?;
+			generate_to(Shell::Fish, &mut app, "gtheme", &completions_dir)?;
+			generate_to(Shell::PowerShell, &mut app, "gtheme", &completions_dir)?;
+			generate_to(Shell::Elvish, &mut app, "gtheme", &completions_dir)?;
 
 			// Generate manpage
-			// let app = app.name("gtheme");
-			// let man = clap_mangen::Man::new(app);
-			// let mut buffer: Vec<u8> = Default::default();
-			// man.render(&mut buffer)?;
-			// std::fs::write(manpage_out_dir.join("gtheme.1"), buffer)?;
+			let app = app.name("gtheme");
+			let man = Man::new(app);
+			let mut buffer: Vec<u8> = Default::default();
+			man.render(&mut buffer)?;
+			std::fs::write(manpage_dir.join("gtheme.1"), buffer)?;
 
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=./src/cli/commands.rs");
