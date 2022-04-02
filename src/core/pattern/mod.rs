4183b34e724c9ea20fc0f8b106b63d6ee1ed6b7a
Jorge Hermo Gonzalez <jorge.hermo.gonzalez@udc.es>
2022-04-02 14:11:41 +0200

    support for pattern submodules (epic shit)

--- src/core/pattern/mod.rs
+++ src/core/pattern/mod.rs
@@ -1,6 +1,7 @@
 use std::collections::HashSet;
-use std::fs::{self,File};
+use std::fs::{self,File,metadata};
 use std::io::prelude::*;
+use std::path::Path;
 use regex::Regex;
 use log::{info,error,warn};
 
@@ -14,17 +15,65 @@ pub struct Pattern {
 	name: String,
 	path: String,
 	output: Option<String>,
-	content: String
+	content: String,
+	submodules: Option<Vec<PatternFile>>
 }
 impl Pattern {
 	//TODO: From str or from PatternFile??
 	pub fn from(pattern: &PatternFile) -> Self {
+
+		let pattern_path = Path::new(pattern.get_path());
+
+		let metadata = match metadata(pattern_path){
+			Ok(metadata) => metadata,
+			Err(e) =>{
+				error!("Could not read metadata from |{}|: |{}|",pattern.get_path(),e);
+				return Self::default(pattern);
+			}
+		};
+
+		if metadata.is_dir(){
+			//Change submodules name to "pattern_name.submodule_name" instead of "submodule_name"
+			let submodules = Self::get_patterns_from_path(pattern_path).into_iter()
+				.map(|pattern_file| PatternFile{
+					path: pattern_file.get_path().to_string(),
+					name: format!("{}.{}",pattern.get_name(),pattern_file.get_name())
+				}).collect();
+			
+			//TODO
+			//Return content as empty string. Maybe better option<String>? lazy to do this change tho
+			return Pattern{
+				name:pattern.get_name().to_string(),
+				path:pattern.get_path().to_string(),
+				output:None,
+				content:"".to_string(),
+				submodules:Some(submodules)
+			}
+		}else if !metadata.is_file(){
+			error!("Pattern |{}| from |{}|is not a directory nor a file",pattern.get_name(),pattern.get_path());
+			return Self::default(pattern);
+		}
+
+		let mut file = match File::open(pattern_path){
+			Ok(file) => file,
+			Err(e) =>{
+				error!("Could not open pattern |{}| from |{}|: |{}|",pattern.get_name(),pattern.get_path(),e);
+				return Self::default(pattern);
+			}
+		};
+
 		let re = Regex::new(r"<\[output-file\]>=(.*)(\r\n|\r|\n)").unwrap();
-		let mut file = File::open(pattern.get_path()).expect(&format!("Error while opening pattern: {}", pattern.get_path()));
 
 		let mut content = String::new();
 		file.read_to_string(&mut content).expect(&format!("Error while reading pattern: {}", pattern.get_path()));
 
+		match file.read_to_string(&mut content){
+			Ok(_) => (),
+			Err(e)=>{
+				error!("Error while reading pattern |{}| from |{}|: |{}|",pattern.get_name(), pattern.get_path(),e);
+				return Self::default(pattern);
+			}
+		}
 		let output_path = match re.captures(&content) {
 			Some(capture) => Some(core::expand_path(&capture[1])),
 			None => None
@@ -35,7 +84,8 @@ impl Pattern {
 			name: String::from(pattern.get_name()),
 			path: String::from(pattern.get_path()),
 			output: output_path,
-			content
+			content,
+			submodules:None
 		}
 	}
 
@@ -47,6 +97,21 @@ impl Pattern {
 	}
 	pub fn get_output(&self) -> &Option<String> 	{
 		&self.output
+	}
+	pub fn has_submodules(&self)->bool{
+		self.submodules.is_some()
+	}
+	pub fn get_submodules(&self) -> &Option<Vec<PatternFile>>{
+		&self.submodules
+	}
+	fn default(pattern: &PatternFile) ->Self{
+		Pattern {
+			name: String::from(pattern.get_name()),
+			path: String::from(pattern.get_path()),
+			output: None,
+			content:"".to_string(),
+			submodules:None
+		}
 	}
 
 	pub fn get_by_name(desktop: &DesktopFile, pattern: &str) -> Option<PatternFile> {
@@ -62,10 +127,16 @@ impl Pattern {
 
 	pub fn get_patterns(desktop: &DesktopFile) -> Vec<PatternFile> {
 		let patterns_dir = format!("{}/gtheme/patterns", desktop.get_path());
-		let entries = match fs::read_dir(&patterns_dir) {
+		let path = Path::new(&patterns_dir);
+		return Self::get_patterns_from_path(path);
+	}
+
+	fn get_patterns_from_path(path: &Path) -> Vec<PatternFile> {
+		let path_name = path.as_os_str().to_string_lossy();
+		let entries = match fs::read_dir(path) {
 			Ok(dir) => dir,
 			Err(e) => {
-				error!("Could not read directory |{}|: |{}|", &patterns_dir, e);
+				error!("Could not read directory |{}|: |{}|", path_name, e);
 				return vec![]
 			}
 		};
@@ -75,7 +146,7 @@ impl Pattern {
 			let entry = match entry {
 				Ok(entry) => entry,
 				Err(e) => {
-					error!("Error while reading entry from dir |{}|: |{}|", &patterns_dir, e);
+					error!("Error while reading entry from dir |{}|: |{}|", path_name, e);
 					continue;
 				}
 			};
@@ -115,6 +186,15 @@ impl Pattern {
 	pub fn fill(&self, theme: &Theme, is_inverted: bool, user_config: &UserConfig, dry_run: bool) {
 		info!("Filling |{}| pattern with |{}| theme...", self.get_name(), theme.get_name());
 
+		//If there are submodules
+		if let Some(submodules )= self.get_submodules() {
+			for submodule in submodules{
+				submodule.to_pattern().fill(theme,is_inverted,user_config,dry_run);
+			}
+			return;
+		}
+		
+		// if pattern has no submodules (i.e, is a file)
 		let filled_content = self.fill_values(theme, is_inverted, user_config);
 
 		let output_path = match self.get_output() {
@@ -251,4 +331,6 @@ mod tests{
 			dbg!(caps);
 		};		
 	}
+
+
 }
